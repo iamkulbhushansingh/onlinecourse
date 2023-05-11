b412436b492a38e426bd7c95cf041c1992e09390
iamkulbhushansingh <iamkulbhushansingh@gmail.com>
2023-05-11 15:59:11 +0530

    config file error fixed

--- onlinecourse/admin/getCourseCode.php
+++ onlinecourse/admin/getCourseCode.php
@@ -1,5 +1,5 @@
 <?php
-include('config.php');
+include('includes/config.php');
 $courseName = $_GET['courseName'];
 $sql = "SELECT course_code FROM course WHERE courseName = '".mysqli_real_escape_string($con, $courseName)."'";
 $result = mysqli_query($con, $sql);
